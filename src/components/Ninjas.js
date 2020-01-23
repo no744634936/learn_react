fce6d8ad6254040726eae695c19873bb481d1b96
no744634936 <zhanghaifeng1123@gmail.com>
2020-01-23 23:02:16 +0900

    stateless component

---
+++ src/components/Ninjas.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+//funcitonal component 不用导入{ Component }  没有state，没有render方法,没有this
+const Ninjas=(props)=>{
+    // console.log(this.props);
+    const { ninjas } = props;
+    const ninjaList=ninjas.map((ninja,index)=>{
+      return (
+        <div className="ninja" key={index}>
+          <div>Name: { ninja.name }</div>
+          <div>Age: { ninja.age }</div>
+          <div>Belt: {ninja.belt }</div>
+        </div>
+      )
+    });
+
+    return(
+      <div className="ninja-list">
+        {ninjaList}
+      </div>
+    )
+}
+
+export default Ninjas
