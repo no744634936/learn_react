f23d2cb6eefa2b690022d755066b8697b1e9f64d
no744634936 <zhanghaifeng1123@gmail.com>
2020-01-23 23:23:50 +0900

    first commit

--- src/components/Ninjas.js
+++ src/components/Ninjas.js
@@ -2,16 +2,27 @@ import React from 'react'
 
 //funcitonal component 不用导入{ Component }  没有state，没有render方法,没有this
 const Ninjas=(props)=>{
-    // console.log(this.props);
     const { ninjas } = props;
+    // const ninjaList=ninjas.map((ninja,index)=>{
+    //     if(ninja.age>15){
+    //         return (
+    //             <div className="ninja" key={index}>
+    //               <div>Name: { ninja.name }</div>
+    //               <div>Age: { ninja.age }</div>
+    //               <div>Belt: {ninja.belt }</div>
+    //             </div>
+    //         )  
+    //     }
+    // });
     const ninjaList=ninjas.map((ninja,index)=>{
-      return (
-        <div className="ninja" key={index}>
-          <div>Name: { ninja.name }</div>
-          <div>Age: { ninja.age }</div>
-          <div>Belt: {ninja.belt }</div>
-        </div>
-      )
+        return ninja.age>15?
+             (
+                <div className="ninja" key={index}>
+                  <div>Name: { ninja.name }</div>
+                  <div>Age: { ninja.age }</div>
+                  <div>Belt: {ninja.belt }</div>
+                </div>
+            ):null;
     });
 
     return(
