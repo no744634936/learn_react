6cb86933a66d35493b43832ba0b54f8e40d02025
no744634936 <zhanghaifeng1123@gmail.com>
2020-02-01 12:52:25 +0900

    add explaination

--- src/Component/Post.js
+++ src/Component/Post.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react'
-import axios from "axios"
+import {connect} from "react-redux"
 
 export class Post extends Component {
-    state={
-        post:null
-    }
-    
-    componentDidMount(){
-        let id=this.props.match.params.post_id
-        axios.get("https://jsonplaceholder.typicode.com/posts/"+id)
-            .then(res=>{
-                this.setState({
-                    post:res.data
-                })
-            })
-    }
     render() {
-        const post=this.state.post?(
+        console.log(this.props);
+        
+        const post=this.props.post?(
             <div>
-                <h4 className="center">{this.state.post.title}</h4>
-                <p>{this.state.post.body}</p>
+                <h4 className="center">{this.props.post.title}</h4>
+                <p>{this.props.post.body}</p>
             </div>
         ):(
             <div className="center">no data</div>
@@ -32,4 +21,18 @@ export class Post extends Component {
     }
 }
 
-export default Post
+//ownProps 是这个component本身自带的props
+const mapStoreStateToProps=(state,ownProps)=>{
+
+    
+    //注意match.params.post_id 取出的id 是字符型。而state.posts里面的是数字。
+    let id=parseInt(ownProps.match.params.post_id);
+
+    let post= state.posts.find(post=>post.id===id)
+    return ({
+        post: post,
+    })
+}
+
+//这样 Post component就能用this.props.post 接触到 mapStoreStateToProps 方法return的内容了
+export default connect(mapStoreStateToProps)(Post)
