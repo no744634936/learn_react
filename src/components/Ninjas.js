2938494e9eefa2b38f448a5352391227e9e00bc9
no744634936 <33306731+no744634936@users.noreply.github.com>
2020-05-21 14:42:53 +0900

    Update Ninjas.js

--- src/components/Ninjas.js
+++ src/components/Ninjas.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-//funcitonal component 不用导入{ Component }  没有state，没有render方法,没有this
+//funcitonal component (也叫ui component)不用导入{ Component }  没有state，没有render方法,没有this
 //就只是一个方法。
 const Ninjas=(props)=>{
     // console.log(this.props);
     const { ninjas } = props;
-    const ninjaList=ninjas.map((ninja,index)=>{
+    const ninjaList=ninjas.map((ninja,index)=>{component
       return (
         <div className="ninja" key={index}>
           <div>Name: { ninja.name }</div>
